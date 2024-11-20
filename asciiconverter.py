ef19dd5003f501fb916311521144678c919798c4
miracleflame <miracle.flame@gmail.com>
2024-11-20 10:06:04 +0100

    correction

--- asciiconverter.py
+++ asciiconverter.py
@@ -1,5 +1,5 @@
 from unidecode import unidecode
 
-with open('.\\English-resources-puv.assets-894.txt', mode='r', encoding='utf8') as fin:
+with open('.\\English-resources.assets-894.txt', mode='r', encoding='utf8') as fin:
     deaccented = unidecode(fin.read())
     open('.\\deaccentedEnglish-resources.assets-894.txt', mode='w', encoding='utf8').write(deaccented)
